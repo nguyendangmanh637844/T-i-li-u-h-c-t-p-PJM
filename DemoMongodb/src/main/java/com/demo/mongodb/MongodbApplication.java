bc484177ee38a1931bdb97d5f1cf0abe140f2785
PVCB\manhnd5 <manhnd5@pvcombank.com.vn>
2023-12-25 16:10:13 +0700

    Khong cần @EnableMongoRepositories

--- DemoMongodb/src/main/java/com/demo/mongodb/MongodbApplication.java
+++ DemoMongodb/src/main/java/com/demo/mongodb/MongodbApplication.java
@@ -2,14 +2,12 @@ package com.demo.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class MongodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
 
 }
