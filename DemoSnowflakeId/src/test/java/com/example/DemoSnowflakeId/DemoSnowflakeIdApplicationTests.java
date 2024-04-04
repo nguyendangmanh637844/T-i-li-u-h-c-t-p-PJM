4d7f4acd7133eb278bd5c5ad63dafdd6b29f0fdc
PVCB\manhnd5 <manhnd5@pvcombank.com.vn>
2024-04-04 11:18:39 +0700

    Thêm demo sử dụng snowflakeId

---
+++ DemoSnowflakeId/src/test/java/com/example/DemoSnowflakeId/DemoSnowflakeIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoSnowflakeId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSnowflakeIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
